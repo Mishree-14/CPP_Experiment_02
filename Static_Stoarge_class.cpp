00445bd8a0edd71455967ce949db814721b1da38
Mishree_14 <mishreekalaria14@gmail.com>
2025-07-19 22:49:23 +0530

    Create Static_Stoarge_class.cpp

---
+++ Static_Stoarge_class.cpp
@@ -0,0 +1,26 @@
+//Mishree Kalaria
+//24070123052
+//Batch:A2
+
+#include<iostream>
+using namespace std;
+
+int a =5;
+void func(){
+    static int a;
+    // We utilise the default value 0. THe variable will hold the value irrespective of the number of function calls.
+    cout<<"Address of func variable a: "<<&a<<endl;
+    a++;
+    cout<<"Size of variable: "<<sizeof(a)<<endl;
+    cout<<"The variable is :"<<a<<endl;
+    cout<<endl;
+}
+
+int main(){
+    func();
+    func();
+    func();
+    cout<<"The variable is :"<<a<<endl;
+    cout<<"Address of global variable a: "<<&a;
+    return 0;
+}
